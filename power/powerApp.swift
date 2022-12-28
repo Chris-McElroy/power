93d0508f3b596db2eb40c703821f80ce27c39249
ChrisMcElroy <csmcelroy@me.com>
2022-12-28 01:30:59 -0500

    Initial Commit

---
+++ power/powerApp.swift
@@ -0,0 +1,17 @@
+//
+//  powerApp.swift
+//  power
+//
+//  Created by Chris McElroy on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct powerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
