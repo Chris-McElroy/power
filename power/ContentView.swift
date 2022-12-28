89004bfcbde95f9803716c81a31a0d445ddf8838
ChrisMcElroy <csmcelroy@me.com>
2022-12-28 02:52:59 -0500

    first draft

--- power/ContentView.swift
+++ power/ContentView.swift
@@ -5,22 +5,59 @@
 //  Created by Chris McElroy on 12/28/22.
 //
 
+import IOKit.ps
 import SwiftUI
 
+enum PowerState {
+	case dying, low, fine, charged
+}
+
 struct ContentView: View {
+	@State var powerState = getPowerState()
+	
+	var color: Color {
+		switch powerState {
+		case .fine: return .clear
+		case .charged: return Color(.displayP3, red: 0, green: 0.85, blue: 0.3)
+		case .low: return Color(.displayP3, red: 0.88, green: 0.62, blue: 0.0, opacity: 1.0)
+		case .dying: return Color(.displayP3, red: 1, green: 0.0, blue: 0.0, opacity: 1.0)
+		}
+	}
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		Circle()
+			.foregroundColor(color)
+			.frame(width: 3, height: 3)
+			.padding(.all, 3)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+
+// from https://stackoverflow.com/a/34571839/8222178
+func getPowerState() -> PowerState {
+	// Take a snapshot of all the power source info
+	guard let snapshot = IOPSCopyPowerSourcesInfo()?.takeRetainedValue() else { return .dying }
+
+	// Pull out a list of power sources
+	guard let sources = IOPSCopyPowerSourcesList(snapshot)?.takeRetainedValue() as? [AnyObject] else { return .dying }
+	
+	// For each power source...
+	for ps in sources {
+		// Fetch the information for a given power source out of our snapshot
+		guard let info = IOPSGetPowerSourceDescription(snapshot, ps).takeUnretainedValue() as? [String: AnyObject] else { return .dying }
+		
+		guard (info[kIOPSNameKey] as? String) == "InternalBattery-0" else { return .dying }
+		
+		let capacity = (info[kIOPSCurrentCapacityKey] as? Int) ?? 0
+		
+		if (info[kIOPSPowerSourceStateKey] as? String) == "AC Power" && capacity >= 80 {
+			return .charged
+		}
+		
+		if capacity <= 20 { return .dying }
+		if capacity < 50 { return .low }
+		return .fine
+	}
+	
+	return .dying
 }
--- power/powerApp.swift
+++ power/powerApp.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 @main
 struct powerApp: App {
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	
     var body: some Scene {
-        WindowGroup {
+		WindowGroup(id: "main") {
             ContentView()
         }
+		.windowResizability(.contentSize)
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+	func applicationWillFinishLaunching(_ notification: Notification) {
+		// https://stackoverflow.com/questions/70091919/how-set-position-of-window-on-the-desktop-in-swiftui
+		UserDefaults.standard.set("0 0 9 37 0 0 1512 950 ", forKey: "NSWindow Frame main-AppWindow-1")
+	}
+	
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		if let window = NSApplication.shared.windows.first {
+			window.titleVisibility = .hidden
+			window.titlebarAppearsTransparent = true
+			window.standardWindowButton(NSWindow.ButtonType.closeButton)!.isHidden = true
+			window.standardWindowButton(NSWindow.ButtonType.miniaturizeButton)!.isHidden = true
+			window.standardWindowButton(NSWindow.ButtonType.zoomButton)!.isHidden = true
+			window.isOpaque = false
+			window.hasShadow = false
+			window.level = .floating
+			window.backgroundColor = NSColor.clear
+			window.isReleasedWhenClosed = false
+			window.isMovableByWindowBackground = false
+			window.collectionBehavior = .canJoinAllSpaces
+			window.titlebarSeparatorStyle = .none
+			window.ignoresMouseEvents = true
+			window.delegate = self
+		}
+		
+		return
+	}
 }
